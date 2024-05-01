b7e1ea06c2b232f65c3ddedb4ea9e8e6117fe10d
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-01 20:22:50 -0300

    User model

---
+++ src/models/user.ts
@@ -0,0 +1,23 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface IUser extends Document {
+    nome: string;
+    email: string;
+    idade: number;
+    genero: string;
+    telefone: string;
+    cpf: string;
+    rg: string;
+}
+
+const userSchema = new Schema({
+    nome: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    idade: { type: Number, required: true },
+    genero: { type: String, required: true },
+    telefone: { type: String },
+    cpf: { type: String, required: true, unique: true },
+    rg: { type: String, required: true, unique: true },
+});
+
+export default model<IUser>('User', userSchema);
